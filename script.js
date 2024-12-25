ba42eb5be9f71c3170c432a85b87e4e21bab3ed8
RohitSaharan0398 <rohit8607745768@gmail.com>
2024-12-25 12:34:45 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
 //your JS code here. If required.
+function daysOfAYear(num){
+	let isLeapYear=false;
+	if(num%4==0){
+		if(num%100==0){
+			if(num%400==0){
+				isLeapYear=true;
+			}
+		}
+		isLeapYear=true;
+	}
+	
+	if(isLeapYear)
+		return 366;
+	return 365
+}
+let year =prompt("Please enter your year:");
+alert(daysOfAYear(year));
